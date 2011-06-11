7ddbfd54161769a67c4ea7850c818e310ed6cbf7
Nick <nfisikelli@gmail.com>
2011-06-11 16:51:26 -0700

    Implemented deletion functionality for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -36,6 +36,13 @@ class ProjectsController < ApplicationController
    flash[:alert] = "Project has not been updated."
    render :action => "edit"
   end
-end
+ end 
+
+ def destroy
+  @project = Project.find(params[:id])
+  @project.destroy
+  flash[:notice] = "Project has been deleted."
+  redirect_to projects_path
+ end
 
 end
