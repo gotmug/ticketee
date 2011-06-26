a82b92b42c28131c7685626f3e0ddb2b51414375
Nick <nfisikelli@gmail.com>
2011-06-26 13:53:06 -0700

    Made projects only visible to users with permission to see them

---
+++ app/models/permission.rb
@@ -0,0 +1,5 @@
+class Permission < ActiveRecord::Base
+  belongs_to :user
+  #belongs_to :project
+  belongs_to :object, :polymorphic => true
+end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -7,6 +7,7 @@ describe ProjectsController do
  let(:project) { Factory(:project) }
 
  it "displays an error message when asked for a missing project" do
+  sign_in(:user, user)
   get :show, :id => "not-here"
   response.should redirect_to(projects_path)
   flash[:alert].should eql("The project you were looking for could not be found.")
@@ -24,7 +25,14 @@ describe ProjectsController do
     response.should redirect_to(root_path)
     flash[:alert].should eql("You must be an admin to do that.")
    end
+   
   end
+  it "cannot access the show action" do
+     sign_in(:user, user)
+     get :show, :id => project.id
+     response.should redirect_to(projects_path)
+     flash[:alert].should eql("The project you were looking for could not be found.")
+   end
  end
 
 end
